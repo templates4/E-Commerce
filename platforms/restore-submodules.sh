36dfa0017000f2e7571bc07f6c55e6f11b6daaf3
Scott Weeden <git@websharpstudios.com>
2023-06-29 09:19:52 -0400

    modified:   .gitmodules 	new file:   platforms/SuperFlexiUI 	modified:   platforms/restore-submodules.sh

--- platforms/restore-submodules.sh
+++ platforms/restore-submodules.sh
@@ -7,11 +7,12 @@ cd /Users/owner/Projects/E-Commerce/platforms
 # git submodule update --init --recursive
 
 # git submodule add --name Orchard -- https://github.com/templates4/Orchard ./Orchard
-git submodule add --name Kooboo -- https://github.com/templates4/Kooboo ./Kooboo
-git submodule add --name Piranha -- https://github.com/templates4/piranha ./Piranha
-git submodule add --name N2 -- https://github.com/templates4/n2 ./N2
-git submodule add --name Component -- https://github.com/templates4/Component ./Component
-git submodule add --name VirtoCommerce -- https://github.com/templates4/VirtoCommerce ./VirtoCommerce
-git submodule add --name SmartStore -- https://github.com/templates4/SmartStore ./SmartStore     
-git submodule add --name GrandNode -- https://github.com/templates4/GrandNode ./GrandNode 
-git submodule add --name Umbraco -- https://github.com/templates4/Umbraco ./Umbraco
+# git submodule add --name Kooboo -- https://github.com/templates4/Kooboo ./Kooboo
+# git submodule add --name Piranha -- https://github.com/templates4/piranha ./Piranha
+# git submodule add --name N2 -- https://github.com/templates4/n2 ./N2
+# git submodule add --name Component -- https://github.com/templates4/Component ./Component
+# git submodule add --name VirtoCommerce -- https://github.com/templates4/VirtoCommerce ./VirtoCommerce
+# git submodule add --name SmartStore -- https://github.com/templates4/SmartStore ./SmartStore     
+# git submodule add --name GrandNode -- https://github.com/templates4/GrandNode ./GrandNode 
+# git submodule add --name Umbraco -- https://github.com/templates4/Umbraco ./Umbraco
+git submodule add --name SuperFlexiUI -- https://github.com/templates4/SuperFlexiUI ./SuperFlexiUI
