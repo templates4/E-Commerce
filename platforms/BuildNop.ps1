e9c2b7105225d7ea320b7355916e45a8319037e4
Scott Douglas \Weeden <scott@websharpstudios.com>
2023-07-09 02:34:44 -0400

    Adding Nop Commerce basic template

---
+++ platforms/BuildNop.ps1
@@ -0,0 +1,36 @@
+﻿
+
+function buildPath($pPath = "C:\a\platforms\Nop"){
+# Find all .csproj files in the parent directory and subdirectories
+$csprojFiles = Get-ChildItem -Path $pPath -Filter *.csproj -Recurse
+
+# Loop through each .csproj file
+foreach($csprojFile in $csprojFiles)
+{
+        Write-Host $csprojFile.FullName
+        # Extract directory path
+        //$directoryPath = [System.IO.Path]::GetDirectoryName($csprojFile)
+
+        //# Change to the directory of the csproj file
+        Set-Location -Path $directoryPath
+
+        # Run dotnet build
+        dotnet build $csprojFile.FullName
+
+        # Run dotnet publish
+        dotnet publish $csprojFile.FullName
+
+        # Change back to the original directory
+        Set-Location -Path $ParentDirectoryPath
+}
+$directoryPath = [System.IO.Path]::GetDirectoryName($csprojFile)
+
+      //# Change to the directory of the csproj file
+        Set-Location -Path $directoryPath
+        foreach($dir in $directoryPath) {
+           buildPath($dir);
+           }
+}
+
+buildPath("C:\a\platforms\Nop")
+
---
+++ platforms/Publish.ps1
@@ -0,0 +1,26 @@
+﻿param(
+  [string]$PlatformRootPath
+)
+
+Write-Host "Publishing artifacts from: $PlatformRootPath"
+
+
+function buildPath($pPath = ""){
+# Find all .csproj files in the parent directory and subdirectories
+  $csprojFiles = Get-ChildItem -Path $pPath -Filter *.csproj -Recurse
+
+# Loop through each .csproj file
+  foreach($csprojFile in $csprojFiles)
+    {
+        Write-Host "Publishing $($csprojFile.FullName)"
+        # Extract directory path
+
+        # Run dotnet publish
+        dotnet publish $csprojFile.FullName
+
+    }
+   
+ }
+
+buildPath($PlatformRootPath)
+
